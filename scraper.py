0e30a6ad750ef81ed5822ffd11785f736004fb21
Raffael Vogler <rv@yieldlove.de>
2015-11-30 15:48:30 +0100

    not necessary to loop on

--- scraper.py
+++ scraper.py
@@ -87,13 +87,11 @@ def scrap(url):
 
 
 def redondance(l):
-    a=False
     for i in range(0,len(l)-2):
         for j in range(i+1,len(l)-1):
             if l[i]==l[j] :
-                a=True
-
-    return a
+                return True
+    return False
 
 def suppredon(l):
     l1=[]
